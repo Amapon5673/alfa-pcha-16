af0f47822b9d104f14f1bac4f81f5551a3e5fd78
Amapon5673 <89888610+Amapon5673@users.noreply.github.com>
2021-09-08 10:27:05 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ app.component('answer-input', {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
       okMessage: '正解！',
-      ngMessage: '？？？？？？？？？？？？？？',
+      ngMessage: '？？？？？？？？？？？？？？┐(´д｀)┌？？？？？？？？？？？？？？',
       message: '',
       inputAnswer: '',
     }
